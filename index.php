00aba99d3041aca45fbfa6baab651fc57254f3c5
Jan Schädlich <schaedlich.jan@gmail.com>
2013-09-28 12:10:41 +0200

    Moved doctrine bootstrap code to di container

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
 require_once realpath(dirname(__FILE__)) . '/vendor/autoload.php';
 
-$paths = array('services/vo');
-$isDevMode = false;
-$entityManagerConfig = Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-
-$databaseConfig = array('driver' => 'pdo_mysql', 'user' => 'foo', 'password' => 'foo', 'dbname' => 'foo');
-$entityManager = Doctrine\ORM\EntityManager::create($databaseConfig, $entityManagerConfig);
-
 $config = new Amfphp_Core_Config();
 $config->serviceFolderPaths = array(dirname(__FILE__) . '/services/');
 $config->pluginsConfig['AmfphpCustomClassConverter'] = array('customClassFolderPaths' => array(dirname(__FILE__) . '/services/vo'));
---
+++ services/DIContainer.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use \Doctrine\ORM\Tools\Setup;
+use \Doctrine\ORM\EntityManager;
+
+class DIContainer extends Pimple
+{
+	public function __construct() {
+		$this['isDevMode'] = false;
+		$this['domainObjectsPath'] =  array('services/vo');
+		$this['databaseConfig'] = array(
+			'driver' => 'pdo_mysql', 'user' => 'foo', 
+			'password' => 'foo', 'dbname' => 'foo');
+		
+		$this['entityManagerConfig'] = function ($c) {
+			return Setup::createAnnotationMetadataConfiguration(
+					$c['domainObjectsPath'], 
+					$c['isDevMode']);
+		};
+		$this['entityManager'] = $this->share(function ($c) {
+			return EntityManager::create(
+					$c['databaseConfig'], 
+					$c['entityManagerConfig']);
+		});
+	}
+}
